ce409ce6be6d29bf7e80200da05bff5d263b1e9d
ratheeshri1984 <ratheeshri1984@gmail.com>
2026-01-30 11:49:50 +0000

    P2-2562_fix

--- src/SFA.DAS.DigitalCertificates.Infrastructure/Configuration/DigitalCertificatesWebConfiguration.cs
+++ src/SFA.DAS.DigitalCertificates.Infrastructure/Configuration/DigitalCertificatesWebConfiguration.cs
@@ -8,7 +8,7 @@ namespace SFA.DAS.DigitalCertificates.Infrastructure.Configuration
         public required string ServiceBaseUrl { get; set; }
         public required string RedisConnectionString { get; set; }
         public required string DataProtectionKeysDatabase { get; set; }
-        public int? SharingListLimit { get; set; }
-        public int? SharingHistoryLimit { get; set; }
+        public int SharingListLimit { get; set; }
+        public int SharingHistoryLimit { get; set; }
     }
 }
