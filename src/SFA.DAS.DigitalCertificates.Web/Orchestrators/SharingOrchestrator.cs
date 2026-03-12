1064d181de5e61f4b899e551e381043cf8ea04b8
ratheeshri1984 <ratheeshri1984@gmail.com>
2026-03-12 19:53:10 +0000

    P2-2563-Handled response empty

--- src/SFA.DAS.DigitalCertificates.Web/Orchestrators/SharingOrchestrator.cs
+++ src/SFA.DAS.DigitalCertificates.Web/Orchestrators/SharingOrchestrator.cs
@@ -67,30 +67,22 @@ namespace SFA.DAS.DigitalCertificates.Web.Orchestrators
                 Limit = _digitalCertificatesWebConfiguration.SharingListLimit == 0 ? null : _digitalCertificatesWebConfiguration.SharingListLimit
             });
 
-            if (response == null)
-            {
-                return new CreateCertificateSharingViewModel
+            var sharings = response?.Sharings?
+                .Select(s => new CreateCertificateSharingItemViewModel
                 {
-                    CertificateId = certificateId,
-                    CourseName = certificate.CourseName,
-                    CertificateType = certificate.CertificateType,
-                    Sharings = new List<CreateCertificateSharingItemViewModel>()
-                };
-            }
+                    SharingId = s.SharingId,
+                    SharingNumber = s.SharingNumber,
+                    CreatedAt = s.CreatedAt,
+                    ExpiryTime = s.ExpiryTime
+                })
+                .ToList() ?? new List<CreateCertificateSharingItemViewModel>();
 
             return new CreateCertificateSharingViewModel
             {
                 CertificateId = certificateId,
                 CourseName = certificate.CourseName,
                 CertificateType = certificate.CertificateType,
-                Sharings = response.Sharings?.Select(s => new CreateCertificateSharingItemViewModel
-                {
-                    SharingId = s.SharingId,
-                    SharingNumber = s.SharingNumber,
-                    CreatedAt = s.CreatedAt,
-                    ExpiryTime = s.ExpiryTime
-                }).ToList()
-                ?? new List<CreateCertificateSharingItemViewModel>()
+                Sharings = sharings
             };
         }
 
