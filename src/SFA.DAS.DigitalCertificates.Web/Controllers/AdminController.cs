cbb316189d8fecaca9cbf933f4ae12660a3e0d9e
Chris Woodcock <chris@humesoftware.com>
2026-05-08 20:42:29 +0100

    Add a show matches admin endpoint

--- src/SFA.DAS.DigitalCertificates.Web/Controllers/AdminController.cs
+++ src/SFA.DAS.DigitalCertificates.Web/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using SFA.DAS.DigitalCertificates.Web.Services;
@@ -15,6 +16,7 @@ namespace SFA.DAS.DigitalCertificates.Web.Controllers
 
         #region Routes
         public const string ClearMatchesRouteGet = nameof(ClearMatchesRouteGet);
+        public const string ShowMatchesRouteGet = nameof(ShowMatchesRouteGet);
         #endregion Routes
 
         public AdminController(ICacheService cacheService, IUserService userService,
@@ -27,12 +29,33 @@ namespace SFA.DAS.DigitalCertificates.Web.Controllers
         }
 
         [Route("clear-matches", Name = ClearMatchesRouteGet)]
-        public IActionResult RevertAuthorisation()
+        public async Task<IActionResult> RevertAuthorisation()
         {
             if (!_config.IsRunningInProd())
             {
                 var govUkIdentifier = _userService.GetGovUkIdentifier();
-                _cacheService.ClearMatches(govUkIdentifier);
+                await _cacheService.ClearMatches(govUkIdentifier);
+            }
+
+            return RedirectToRoute(CertificatesController.CertificatesListRouteGet);
+        }
+
+        [Route("show-matches", Name = ShowMatchesRouteGet)]
+        public async Task<IActionResult> ShowMatches()
+        {
+            if (!_config.IsRunningInProd())
+            {
+                var govUkIdentifier = _userService.GetGovUkIdentifier();
+                var matches = await _cacheService.GetOrCreateMatchesAsync(govUkIdentifier, 
+                    _userService.GetUserId().GetValueOrDefault());
+
+                var matchFailCount = await _cacheService.GetMatchFailCountAsync(govUkIdentifier);
+
+                return Json(new
+                {
+                    matches,
+                    matchFailCount
+                });
             }
 
             return RedirectToRoute(CertificatesController.CertificatesListRouteGet);
